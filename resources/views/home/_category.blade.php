6404389c7c145a25d286b2b8779b705ebd849069
aysenurozbay <aysenurozbay1004@gmail.com>
2020-12-06 23:49:42 +0300

    First version

---
+++ resources/views/home/_category.blade.php
@@ -0,0 +1,42 @@
+<div class="header-bottom">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg p-0">
+            <a class="site-logo site-title" href="index.html"><img src="assets/images/logo1.png" alt="site-logo"><span class="logo-icon"><i class="flaticon-fire"></i></span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="menu-toggle"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav main-menu mr-auto">
+                    <li class="menu_has_children"><a href="#0">Home</a>
+                        <ul class="sub-menu">
+                            <li><a href="index.html">Home One</a></li>
+                            <li><a href="home-two.html">Home Two</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="about.html">About</a>
+                    <li class="menu_has_children"><a href="#0">cars</a>
+                        <ul class="sub-menu">
+                            <li><a href="car-list-one.html">car list one</a></li>
+                            <li><a href="car-list-two.html">car list two</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu_has_children"><a href="#0">pages</a>
+                        <ul class="sub-menu">
+                            <li><a href="reservation.html">reservation</a></li>
+                            <li><a href="login.html">login</a></li>
+                            <li><a href="registration.html">registration</a></li>
+                            <li><a href="error-404.html">404</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu_has_children"><a href="#0">blog</a>
+                        <ul class="sub-menu">
+                            <li><a href="blog.html">blog page</a></li>
+                            <li><a href="blog-details.html">blog details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="contact.html">contact us</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div><!-- header-bottom end -->
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Renten - Car Rental Service HTML Template</title>
+    <!-- site favicon -->
+    <link rel="shortcut icon" type="image/png" href="{{asset('assets')}}/assets/images/favicon.jpg"/>
+    <!-- fontawesome css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/fontawesome.min.css">
+    <!-- bootstrap css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/bootstrap.min.css">
+    <!-- lightcase css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/lightcase.css">
+    <!-- animate css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/animate.css">
+    <!-- nice select css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/nice-select.css">
+    <!-- datepicker css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/datepicker.min.css">
+    <!-- wickedpicker css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/wickedpicker.min.css">
+    <!-- jquery ui css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/jquery-ui.min.css">
+    <!-- owl carousel css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/owl.carousel.min.css">
+    <!-- main style css link -->
+    <link rel="stylesheet" href="{{asset('assets')}}/assets/css/main.css">
+
+    <!-- jquery js link -->
+    <script src="{{asset('assets')}}/assets/js/jquery-3.3.1.min.js"></script>
+    <!-- jquery migrate js link -->
+    <script src="{{asset('assets')}}/assets/js/jquery-migrate-3.0.0.js"></script>
+    <!-- bootstrap js link -->
+    <script src="{{asset('assets')}}/assets/js/bootstrap.min.js"></script>
+    <!-- lightcase js link -->
+    <script src="{{asset('assets')}}/assets/js/lightcase.js"></script>
+    <!-- wow js link -->
+    <script src="{{asset('assets')}}/assets/js/wow.min.js"></script>
+    <!-- nice select js link -->
+    <script src="{{asset('assets')}}/assets/js/jquery.nice-select.min.js"></script>
+    <!-- datepicker js link -->
+    <script src="{{asset('assets')}}/assets/js/datepicker.min.js"></script>
+    <script src="{{asset('assets')}}/assets/js/datepicker.en.js"></script>
+    <!-- wickedpicker js link -->
+    <script src="{{asset('assets')}}/assets/js/wickedpicker.min.js"></script>
+    <!-- owl carousel js link -->
+    <script src="{{asset('assets')}}/assets/js/owl.carousel.min.js"></script>
+    <!-- jquery ui js link -->
+    <script src="{{asset('assets')}}/assets/js/jquery-ui.min.js"></script>
+    <!-- main js link -->
+    <script src="{{asset('assets')}}/assets/js/main.js"></script>
+
+
+    @yield('headercss')
+    @yield('headerjs')
+
+
+</head>
+
+<body>
+
+<!--header -->
+@include('home._header')
+
+
+
+
+    @include('home._slider')
+
+
+
+<!-- burda menü kısmını yaptık -->
+
+
+@section('content')
+
+@show
+<!--footer -->
+@include('home._footer')
+@yield('footerjs')
+
+
+
+
+</body>
+
+</html>
